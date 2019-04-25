5d5d79aedf5f0bce6a2bb02e975fc0bf3a471390
Lucifer-is-my-pet <dasha1994@list.ru>
2019-04-25 19:17:57 +0500

    активити не дохнет, ура
    вынос assert'ов

--- app/src/androidTest/java/com/fintech/fasthub/FastHubApp.java
+++ app/src/androidTest/java/com/fintech/fasthub/FastHubApp.java
@@ -14,41 +14,52 @@ import android.support.test.uiautomator.UiSelector;
 import android.support.test.uiautomator.Until;
 import android.widget.ImageView;
 
+import com.fastaccess.App;
+
+import java.io.File;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
+import static java.lang.Thread.sleep;
 import static org.junit.Assert.assertTrue;
 
 public class FastHubApp {
 
     private static final String PACKAGE = "com.fastaccess.github.debug";
+    private static final String APP_NAME = "FastHub Debug";
     private static final long TIMEOUT = TimeUnit.SECONDS.toMillis(5);
     private final UiDevice device;
     private final Context context;
     private final Intent intent;
     private String[] config;
+    String RESOURCES_PATH = new StringBuilder().append(".").append(File.separator).append("app").append(File.separator).append("src")
+                    .append(File.separator).append("androidTest").append(File.separator).append("java")
+            .append(File.separator).append("com").append(File.separator).append("fintech")
+            .append(File.separator).append("fasthub").append(File.separator).toString();
 
     public FastHubApp(UiDevice device) {
         this.device = device;
         this.context = InstrumentationRegistry.getInstrumentation().getContext();
         this.intent = context.getPackageManager().getLaunchIntentForPackage(PACKAGE);
-        this.config = new FileReaderToArray().readLines("config.txt");
+        this.config = new FileReaderToArray().readLines(RESOURCES_PATH + "config.txt");
+        System.out.println();
     }
 
-    void open() throws TimeoutException {
+    void open() throws TimeoutException, InterruptedException {
         if (!device.wait(Until.hasObject(By.pkg(device.getLauncherPackageName()).depth(0)), TIMEOUT)) {
             throw new TimeoutException("Нет пакета " + device.getLauncherPackageName());
         }
 
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK |Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
         context.startActivity(intent);
+        sleep(300);
 
         if (!device.wait(Until.hasObject(By.pkg(PACKAGE)), TIMEOUT)) {
             throw new TimeoutException("Нет пакета " + PACKAGE);
         }
 
-        if (device.hasObject(By.res(PACKAGE + ":id/mainCard"))) {
+        if (device.hasObject(byId("mainCard"))) {
             login();
         }
     }
@@ -61,35 +72,28 @@ public class FastHubApp {
         assertTrue(hasObjectWithText("FastHub Debug"));
     }
 
-    void clickMenuItem(String itemTitle, String submenuTitle) throws UiObjectNotFoundException {
+    void clickMenuItem(String itemTitle) throws UiObjectNotFoundException {
         openMenu();
         scrollToItem(itemTitle);
-        device.findObject(By.res(PACKAGE + ":id/design_menu_item_text").text(itemTitle)).click();
-        assertTrue(device.wait(Until.hasObject(title(submenuTitle)), TIMEOUT));
-    }
-
-    void clickMenuItem(String itemTitle) throws UiObjectNotFoundException {
-        clickMenuItem(itemTitle, itemTitle);
+        device.findObject(byId("design_menu_item_text").text(itemTitle)).click();
     }
 
     private void openMenu() {
-        System.out.println("11111111111111111111");
-        assertTrue(hasObjectWithText("FastHub Debug"));
         device.findObject(By.desc("Navigate up")).click();
     }
 
     void clickApply() {
-        int last = device.findObjects(By.res(PACKAGE + ":id/apply")).size() - 1;
-        device.findObjects(By.res(PACKAGE + ":id/apply")).get(last).click();
+        int last = device.findObjects(byId("apply")).size() - 1;
+        device.findObjects(byId("apply")).get(last).click();
     }
 
     void clickSubmit() {
-        int last = device.findObjects(By.res(PACKAGE + ":id/submit")).size() - 1;
-        device.findObjects(By.res(PACKAGE + ":id/submit")).get(last).click();
+        int last = device.findObjects(byId("submit")).size() - 1;
+        device.findObjects(byId("submit")).get(last).click();
     }
 
     void clickLogin() {
-        device.findObject(By.res(PACKAGE + ":id/login")).click();
+        device.findObject(byId("login")).click();
     }
 
     void clickObjectWithText(String text) {
@@ -111,18 +115,22 @@ public class FastHubApp {
         return device.wait(Until.hasObject(By.text(text)), TIMEOUT);
     }
 
+    boolean hasObjectWithTitle(String title) {
+        return device.wait(Until.hasObject(title(title)), TIMEOUT);
+    }
+
     void setTextToField(String text, String fieldTitle) {
         device.findObject(By.text(fieldTitle)).findObject(By.clazz("android.widget.EditText")).setText(text);
     }
 
     boolean textFieldContainsText(String fieldName, String text) {
-        return device.findObject(By.res(PACKAGE + ":id/" + fieldName.toLowerCase())).getText().contains(text);
+        return device.findObject(byId(fieldName.toLowerCase())).getText().contains(text);
     }
 
     int itemPosition(String listName, String item) {
         int result = 0;
         List<UiObject2> list =
-                device.findObject(By.text(listName)).getParent().findObjects(By.res(PACKAGE + ":id/mal_item_text"));
+                device.findObject(By.text(listName)).getParent().findObjects(byId("mal_item_text"));
         for (UiObject2 listItem : list) {
             if (listItem.getText().equals(item)) {
                 return list.indexOf(listItem) + 1;
@@ -137,6 +145,10 @@ public class FastHubApp {
 
     private BySelector byId(String id) {
         return By.res(PACKAGE + ":id/" + id);
+    }
+
+    String getAppName() {
+        return APP_NAME;
     }
 
 }
--- app/src/androidTest/java/com/fintech/fasthub/FastHubTest.java
+++ app/src/androidTest/java/com/fintech/fasthub/FastHubTest.java
@@ -1,96 +1,69 @@
 package com.fintech.fasthub;
 
-import android.support.test.runner.AndroidJUnit4;
 import android.support.test.uiautomator.UiDevice;
 import android.support.test.uiautomator.UiObjectNotFoundException;
 
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
 import java.util.concurrent.TimeoutException;
 
 import static android.support.test.InstrumentationRegistry.getInstrumentation;
-import static java.lang.Thread.sleep;
-import static org.hamcrest.core.IsNull.notNullValue;
 import static org.junit.Assert.*;
 
-@RunWith(AndroidJUnit4.class)
 public class FastHubTest {
 
     private static FastHubApp app;
 
-    @BeforeClass
-    public static void openAndLogin() throws TimeoutException {
+    @Before
+    public void before() throws TimeoutException, InterruptedException {
         app = new FastHubApp(UiDevice.getInstance(getInstrumentation()));
         app.open();
     }
 
-    @Before
-    public void before() {
-        // открыть главное меню
-//        device = UiDevice.getInstance(getInstrumentation());
-//        assertThat(device, notNullValue());
-    }
-
-    /**
-     * Приоритет: Высокий
-     * Предусловие – Пользователь залогинен в приложение
-     */
     @Test
     public void checkTrendingPage() throws UiObjectNotFoundException {
         app.clickMenuItem("Trending");
+        assertTrue(app.hasObjectWithTitle("Trending"));
     }
 
-    /**
-     * Приоритет: Низкий
-     * Предусловие – Пользователь залогинен в приложение
-     */
     @Test
     public void checkThemeChange() throws UiObjectNotFoundException {
         app.clickMenuItem("Settings");
+        assertTrue(app.hasObjectWithTitle("Settings"));
         app.clickObjectWithText("Theme");
         assertTrue(app.hasObjectWithText("FastHub Premium Features"));
         app.scrollForward(2);
         app.clickApply();
-        assertTrue(app.hasObjectWithText("FastHub")); // цвет не проверить
+        assertTrue(app.hasObjectWithText(app.getAppName())); // цвет не проверить
     }
 
-    /**
-     * Приоритет: Низкий
-     * Предусловие – Пользователь залогинен в приложение
-     */
     @Test
     public void checkRestorePurchases() throws UiObjectNotFoundException {
-        app.clickMenuItem("Restore Purchases", "FastHub");
+        app.clickMenuItem("Restore Purchases");
+        assertTrue(app.hasObjectWithTitle(app.getAppName()));
         assertFalse(app.hasObjectWithText("Menu")); // Intent не проверить
     }
 
-    /**
-     * Приоритет: Средний
-     * Предусловие – Пользователь залогинен в приложение
-     */
     @Test
-    public void checkReportAnIssue() throws UiObjectNotFoundException {
-        app.clickMenuItem("Report an issue", "Submit feedback");
+    public void checkReportAnIssue() throws UiObjectNotFoundException, InterruptedException {
+        app.clickMenuItem("Send feedback");
+        assertTrue(app.hasObjectWithText("You are currently using a debug build"));
+        app.clickObjectWithText("OK");
+        assertTrue(app.hasObjectWithTitle("Submit feedback"));
         app.setTextToField("hello", "Title");
         app.clickObjectWithText("Description");
         app.clickSubmit();
-        assertTrue(app.hasObjectWithText("Submit feedback"));
+        assertTrue(app.hasObjectWithTitle("Submit feedback"));
         assertTrue(app.textFieldContainsText("Description", "Xiaomi"));
         assertTrue(app.textFieldContainsText("Description", "Redmi 3"));
-        app.clickSubmit();
-        assertTrue(app.hasObjectWithText("Successfully submitted")); // Toast с текстом не детектится UI Automator Viewer'ом
+        app.clickSubmit(); // Toast с текстом не детектится UI Automator Viewer'ом
     }
 
-    /**
-     * Приоритет: Средний
-     * Предусловие – Пользователь залогинен в приложение
-     */
     @Test
     public void checkMarkup() throws UiObjectNotFoundException {
-        app.clickMenuItem("About", "FastHub");
+        app.clickMenuItem("About");
+        assertTrue(app.hasObjectWithTitle(app.getAppName()));
         app.scrollToItem("About");
         assertEquals(2, app.itemPosition("About", "Changelog")); // иконку не проверить
     }
